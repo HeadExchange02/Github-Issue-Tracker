774bb9ca5e896a32cdec250ef096816914f2078a
AzmirAntor <antorazmir7@gmail.com>
2026-03-14 00:52:23 +0600

    load data & card

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,7 +1,28 @@
-const loadData = function loadData(){
+console.log("ok")
+const allIssues = () => {
     fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
     .then((response) => response.json())
-    .then((json) => console.log(json))
+    .then((json) => displayIssues(json.data))
 }
 
+const displayIssues = (issues) => {
+    // 1- get the container & empty
+    const issuesContainer = document.getElementById("issues-container");
+    issuesContainer.innerHTML = "";
+    // 2- get into every issue
+    for(let issue of issues){
+        console.log(issue);
+        // 3- create Element
+        const issuesDiv = document.createElement("div");
+        issuesDiv.innerHTML = `
+            <div class ="flex items-center gap-2">
+                <div>
+                    <h3 class="font-semibold text-xl">Issues </h3>
+                </div>
+            </div>
+        `
+        // 4- append into container
+        issuesContainer.append(issuesDiv);
+    }
+}
 
