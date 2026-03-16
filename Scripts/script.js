73e3f3a1a840992a16e02d3badc5642450dc5e30
AzmirAntor <antorazmir7@gmail.com>
2026-03-17 03:36:32 +0600

    display all data functon complete

--- Scripts/script.js
+++ Scripts/script.js
@@ -20,6 +20,39 @@ function renderData(dataList) {
     document.getElementById('issue-total').innerText = `${dataList.length} Issues Found`;
     document.getElementById('open-total').innerText = allData.filter(i => i.status === 'open').length;
     document.getElementById('closed-total').innerText = allData.filter(i => i.status === 'closed').length;
+
+    dataList.forEach(item =>{
+        const isOpen = item.status === 'open';
+        const topBorder = isOpen ? 'border-t-green-500' : 'border-t-purple-500'
+
+        const labelText = (item.label && item.label.trim() !== "") ? item.label : "General Issue";
+        const labelStyle = (item.label && item.label.trim() !== "") ? "bg-blue-100 text-blue-700" : "bg-gray-100 text-gray-500";
+
+        const card = document.createElement('div');
+        card.className = `bg-white border-t-4 ${topBorder} rounded-lg p-5 shadow-sm hover:shadow-lg transition-all cursor-pointer flex flex-col h-full border border-gray-100`;
+        card.onclick = () => getSingleIssue(item.id);
+
+        card.innerHTML = `
+            <div class="flex justify-between items-center mb-3">
+                <span class="${labelStyle} px-3 py-1 rounded-full text-[10px] font-black uppercase border">
+                    ${labelText}
+                </span>
+                <span class="${isOpen ? 'text-green-600' : 'text-purple-600'} text-[10px] font-bold uppercase">
+                    ${isOpen ? '🟢 Open' : '🟣 Closed'}
+                </span>
+            </div>
+            <h2 class="font-bold text-sm mb-2 text-gray-800 line-clamp-2">${item.title}</h2>
+            <p class="text-xs text-gray-500 mb-6 flex-grow line-clamp-3">${item.description}</p>
+            <div class="flex justify-between items-center pt-4 border-t border-gray-50 mt-auto">
+                <div class="flex items-center gap-2">
+                    <div class="w-6 h-6 bg-blue-500 text-white rounded-full flex items-center justify-center text-[10px] font-bold uppercase">${item.author[0]}</div>
+                    <span class="text-[11px] font-bold text-gray-700">${item.author}</span>
+                </div>
+                <span class="text-[9px] bg-gray-100 px-2 py-0.5 rounded text-gray-400 font-bold uppercase">${item.priority}</span>
+            </div>
+        `;
+        container.appendChild(card);
+    })
 }
 
 function changeTab(type, btn) {
@@ -35,9 +68,6 @@ function changeTab(type, btn) {
     if (type === 'all') renderData(allData);
     else renderData(allData.filter(i => i.status === type));
 }
-
-
-
 
 
 allIssues();
